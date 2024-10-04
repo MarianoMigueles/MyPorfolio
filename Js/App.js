75ea1953f972d04710872e44ad2dd47c124db37a
Mariano <164581983+MarianoMigueles@users.noreply.github.com>
2024-10-04 12:17:50 -0300

    feat( background ): the background animation was added.

--- Js/App.js
+++ Js/App.js
@@ -2,10 +2,38 @@ window.addEventListener('wheel', function(e) {
     if (e.deltaY !== 0) {
       e.preventDefault();
   
-      const container = document.querySelector('main');
+      backgroundScroll(e.deltaY)
+    }
+}, { passive: false });
+
+let initialX  = null;
+
+window.addEventListener('touchstart', function(e) {
+  initialX = e.touches[0].clientX;
+}, { passive: false });
+
+window.addEventListener('touchmove', function(e) {
+  if (initialX === null) return;
+
+  const currentX = e.touches[0].clientX;
+  const deltaX = initialX - currentX; 
+
+  backgroundScroll(deltaX); 
+
+  initialX = currentX;
+}, { passive: false });
+
+window.addEventListener('touchend', function(e) {
+    initialY = null;
+}, { passive: true });
+
+function backgroundScroll(deltaY) {
+  const container = document.querySelector('main');
+
+      scrollBackgroundAnimation(deltaY);   
 
       let currentScroll = container.scrollLeft;
-      let targetScroll = currentScroll + e.deltaY;
+      let targetScroll = currentScroll + deltaY;
   
       const smoothScroll = () => {
         currentScroll += (targetScroll - currentScroll) * 0.2;
@@ -17,6 +45,34 @@ window.addEventListener('wheel', function(e) {
       };
   
       smoothScroll();
+}
+
+let backgroundSize = 100;
+let sumValue = 150;
+let subtractionValue = 100;
+
+if(window.innerWidth < 600) {
+    sumValue = 20;
+    subtractionValue = 20;
+}
+
+function scrollBackgroundAnimation(deltaY) {
+    if(deltaY > 0) {
+      backgroundSize += sumValue;
+    } else {
+      backgroundSize -= subtractionValue;
     }
-});
+
+    console.log(backgroundSize);
+
+    if(backgroundSize > 2000) {
+      backgroundSize = 2000;
+    }
+
+    if(backgroundSize < 100) {
+      backgroundSize = 100;
+    }
+
+    document.documentElement.style.setProperty('--js-Scroll-Background-Animation', backgroundSize + 'px');
+}
   
